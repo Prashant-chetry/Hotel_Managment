f33488ef46e7db40dea6665b5037fb7d601ed0d8
Prashant-Chetry <prashantchetry98@gmail.com>
2019-10-26 07:45:03 +0530

    middleware added

--- middleware/jwtToken.js
+++ middleware/jwtToken.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const secrect = config.get('jwtTokenSecret');
+
 module.exports = async(obj = {})=>{
 	try {
 		if (!Object.keys(obj).length) return {success: false, errorMessage: 'empty'};
